e21a0396cfdb654d43ff3cff7c73fa3e1f4f018f
Théo Michel <theo.michel2@outlook.fr>
2022-05-08 16:28:35 +0200

    textAnimation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import DynamicText from "../components/DynamicText";
 import Mouse from "../components/Mouse";
 import Navigation from "../components/Navigation";
 import SocialNetwork from "../components/SocialNetwork";
@@ -7,8 +8,18 @@ const Home = () => {
   return (
     <div>
       <Mouse />
-      <SocialNetwork />
-      <Navigation />
+      <div className="home">
+        <Navigation />
+        <SocialNetwork />
+        <div className="home-main">
+          <div className="main-content">
+            <h1>FS Agency</h1>
+            <h2>
+              <DynamicText />
+            </h2>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
